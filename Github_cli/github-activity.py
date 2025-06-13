e5d0e2262fea5540c23f51d9cab5be70002d9516
Yeruva S Koushik Reddy <yeruvas.koushikreddy@amadeus.com>
2025-06-13 16:42:43 +0530

    git cli updated

--- Github_cli/github-activity.py
+++ Github_cli/github-activity.py
@@ -1,14 +1,13 @@
 import argparse
-import json
-from useractivity import get_user_activity
+from useractivity import get_user_activity, display_event_summary
 
 def main():
     parser = argparse.ArgumentParser(description="GitHub User Activity")
     parser.add_argument("username", help="Github username")
     args = parser.parse_args()
     # Function call to perform the api request
-    user_activity = json.loads(get_user_activity(args.username))
-    
+    user_events = get_user_activity(args.username)
+    display_event_summary(user_events)
 
 if __name__ == "__main__":
     main()
--- Github_cli/useractivity.py
+++ Github_cli/useractivity.py
@@ -15,13 +15,21 @@ HEADERS = {
 def get_user_activity(username):
     url =  f"https://api.github.com/users/{username}/events"
     try:
-        response = requests.get(url, headers=HEADERS)
+        response = requests.get(url, headers=HEADERS, verify=False)
         response.raise_for_status()  # Raise an error for bad responses
-        return response.json()  # Return the JSON response
+        if response.status_code == 200:
+            events = response.json()
+            return events
     except requests.exceptions.RequestException as e:
         print(f"Failed to fetch the user activity: {e}")
         return None
-    
 
+def display_event_summary(events):
+    for event in events:
+        event_type = event.get('type')
+        repo_name = event.get('repo', {}).get('name')
+        created_at = event.get('created_at')
+        print(f"Event: {event_type} | Repo: {repo_name} | Time: {created_at}")
+        
 
 
